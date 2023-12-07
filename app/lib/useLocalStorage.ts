faedf26e0387160802e2c07f3896b7171b3e5bb9
agan-k <koranagan@gmail.com>
2023-12-07 11:30:26 -0500

    check for undefined window to fix build error

--- app/lib/useLocalStorage.ts
+++ app/lib/useLocalStorage.ts
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react';
 export default function useLocalStorage(storageKey: string, fallback: any) {
-  const [storage, setStorage] = useState(getLocalStorage(storageKey) || []);
+  const [storage, setStorage] = useState(
+    typeof window !== "undefined" ? getLocalStorage(storageKey) : '' || []);
 
   function getLocalStorage(storageKey: string) {//to handle typescript error
     return JSON.parse(localStorage.getItem(storageKey) || '')
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import { useState } from 'react';
 import styles from './page.module.css'
-import { fetchFakeFood } from './lib/data';
 import ShoppingList from './ui/shopping-list';
 
 export default function Home() {
